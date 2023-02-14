7bb6ca40758b866157e50ec972524dfa11a0007b
unknown <talhamuhammad413@gmail.com>
2023-02-14 21:07:04 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+if (true) {
+    var age = 23;
+}
+console.log(age);
+if (true) {
+    var age1 = 23;
+}
+console.log(age1);
---
+++ index.ts
@@ -0,0 +1,9 @@
+if(true){
+    var age = 23
+}
+console.log(age)
+
+if(true){
+    let age1=23
+}
+console.log(age1)
